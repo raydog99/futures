b66e77d591a356ca54282b9c06d2a0416f99dcb4
Raymond Deng <1107450@gmail.com>
2023-09-27 22:42:02 -0700

    add blurb from c++14 experiments

--- cpp/include/rpc/server.h
+++ cpp/include/rpc/server.h
@@ -1,5 +1,20 @@
 #pragma once
 
+/*
+C++ dispatch on HOF
+
+template<class F, F f> 
+struct wrapper_impl;
+
+template<class R, class... Args, R(*f)(Args...)>
+struct wrapper_impl<R(*)(Args...), f> {
+    static R wrap(Args... args) {
+        return f(args...);
+    }
+};
+
+*/
+
 namespace rpc{
 	class server{
 	public:
@@ -9,6 +24,6 @@ namespace rpc{
 		void bind(std::string const &name, std::function func)
 		void stop();
 	private:
-		std::unordered_map<std::string, std::function> dispatchTable;
+		std::unordered_map<std::string, std::any> dispatchTable;
 	}
 }

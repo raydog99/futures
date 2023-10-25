8a21eda977a84c07191bdb4665a3bf62c2098e55
Raymond Deng <1107450@gmail.com>
2023-10-24 23:28:20 -0700

    add tcp listen connect success event

---
+++ cpp/lib/async_io/ATcpListeNSuccessEvent.cpp
@@ -0,0 +1,21 @@
+#include "async_io/QueueElementIF.h"
+#include "async_io/ATcpServerSocket.h"
+
+using namespace root::include::async_io;
+using namespace root::include::rpc;
+using namespace root::internal;
+
+class ATcpListenSuccessEvent : public QueueElementIF {
+public:
+    ATcpServerSocket* theSocket;
+
+    ATcpListenSuccessEvent(ATcpServerSocket* sock) : theSocket(sock) {}
+
+    ATcpServerSocket* getSocket() {
+        return theSocket;
+    }
+
+    std::string toString() {
+        return "ATcpListenSuccessEvent [" + theSocket + "]";
+    }
+};
